d8ba00301f1e65219ad9cf92ecb459d81e7394d1
Bishopolis <jonbishop@gmail.com>
2013-02-12 20:38:50 -0800

    sinatra 2 exercise

--- sinatra/application.rb
+++ sinatra/application.rb
@@ -1,45 +1,35 @@
-require "rubygems"
-require "sinatra"
+require 'sinatra'
+require 'data_mapper'
+require_relative 'in_words'
 
-class Integer
-  def in_words
-    single = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]  
-    tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-    if self < 20
-      single[self]
-    elsif self >= 20 && self < 100
-      tens[self / 10] + " " + single[(self) - ((self / 10) * 10)]
-    elsif self >= 100 && self < 1000
-      if self == 100
-        "one hundred"
-      else
-        #single[(self / 100)] + " hundred " + tens[(self - (self / 100) * 100) / 10] + " " + single[(self) - ((self / 10) * 10)].to_s
-        single[(self / 100)] + " hundred " + (self%100).in_words
-      end
-    else
-      big = %w(thousand million billion trillion).unshift("","")
-      a = []
-      self.to_s.split('').reverse.each_slice(3) do |chunk|
-        a << chunk.reverse.join()
-      end
-      a.reverse!
-      (a.first.to_i.in_words.strip + " " + big[ a.length ] + " " + a[1..-1].join.to_i.in_words.strip).strip
-    end
-  end
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
+
+class Exercise
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, Text
+  property :prompt, Text
+  property :method_name, Text
+  property :test, Text
 end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
-  " <html>
-    <body>
-      <form action='/in_words'>
-        <input type='text' name='number'>
-        <input type='submit'>
-      </form>
-    </body>
-  </html>
-  "
+  @exercises = Exercise.all order: :name.asc
+  erb :index
 end
 
-get '/in_words' do
-  params[:number].to_i.in_words
+get '/:id' do
+  @exercise = Exercise.get params[:id]
+  erb :view   
+end
+
+post '/' do
+  exercise = Exercise.new
+  exercise.name        = params[:name]
+  exercise.prompt      = params[:prompt]
+  exercise.method_name = params[:method_name]
+  exercise.save
+  redirect '/'  
 end
---
+++ sinatra/in_words.rb
@@ -0,0 +1,26 @@
+class Integer
+  def in_words
+    single = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]  
+    tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    if self < 20
+      single[self]
+    elsif self >= 20 && self < 100
+      tens[self / 10] + " " + single[(self) - ((self / 10) * 10)]
+    elsif self >= 100 && self < 1000
+      if self == 100
+        "one hundred"
+      else
+        #single[(self / 100)] + " hundred " + tens[(self - (self / 100) * 100) / 10] + " " + single[(self) - ((self / 10) * 10)].to_s
+        single[(self / 100)] + " hundred " + (self%100).in_words
+      end
+    else
+      big = %w(thousand million billion trillion).unshift("","")
+      a = []
+      self.to_s.split('').reverse.each_slice(3) do |chunk|
+        a << chunk.reverse.join()
+      end
+      a.reverse!
+      (a.first.to_i.in_words.strip + " " + big[ a.length ] + " " + a[1..-1].join.to_i.in_words.strip).strip
+    end
+  end
+end
