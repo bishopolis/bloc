a0871a0af061899f79cc909652040dd5161c5249
Bishopolis <jonbishop@gmail.com>
2013-02-22 14:59:14 -0800

    first commit

---
+++ scaffold/posting_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostingApp::Application.config.secret_token = '9a2cdae8a7f4cd7719b0cbb321162d58af50a8816fdaa627d841145cf7cd6326abf92d210e11bef09a28fb4cfe01350b3de823901f13d48e766394a4b378895d'
