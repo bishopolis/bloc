e510b72361a03696ffd436b1fdf983e174c16cdd
Bishopolis <jonbishop@gmail.com>
2013-02-26 17:09:05 -0800

    getting ready for migrations

--- scaffold/test_app/app/models/post.rb
+++ scaffold/test_app/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
-  def hello
- 	 "Hello #{self.name}"
+  	attr_accessible :content, :name, :title
+  	def hello
+ 		 "Hello #{self.name}"
 	end
+	belongs_to :author
 end
